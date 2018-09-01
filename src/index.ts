7052645e3c5dab0c878fb1f2ad94eb2bf119ec72
Sagi Kedmi <sagi@users.noreply.github.com>
2018-09-01 18:59:49 +0300

    add shebang

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import dnstls from 'dns-over-tls';
-import { parse } from './utils';
-
-(async () => {
-  const options = parse(process.argv.splice(2));
-  const r = await dnstls.query(options);
-  process.stdout.write(JSON.stringify(r, null, 2));
-  process.stdout.write('\n');
-})();
